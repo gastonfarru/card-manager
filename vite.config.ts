c7e3f4d6e590e37d48e9dc31938ee8c16e619ce3
Gaston Rodrigo Farru <gastonfarru@gmail.com>
2025-08-02 19:13:04 -0300

    update vite URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/card-manager/', // Cambia 'card-manager' por el nombre de tu repo
+  base: '/cardmanager/', // Cambia 'card-manager' por el nombre de tu repo
 })

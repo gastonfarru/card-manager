025087953491364556c8904ec54703484782da5c
gastonfarru <38308987+gastonfarru@users.noreply.github.com>
2025-08-02 19:17:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/card-manager/', // Cambia 'card-manager' por el nombre de tu repo
+  base: '/card-manager/', // Debe coincidir EXACTAMENTE con el nombre de tu repo
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
